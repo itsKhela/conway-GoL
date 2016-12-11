86624fa722bb7efc220f7f39c133b6250c21cff4
Melchor Vazquez <mel.mvm@gmail.com>
2016-12-11 07:01:12 +0100

    Initial Commit

---
+++ Main.elm
@@ -0,0 +1,147 @@
+module Main exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Array exposing (Array)
+
+
+getNeighbors : Cell -> Board -> List Cell
+getNeighbors (x,y) board =
+  [ (x-1, y+1), (x, y+1), (x+1, y+1)
+  , (x-1,   y),           (x+1,   y)
+  , (x-1, y-1), (x, y-1), (x+1, y-1)
+  ]
+
+getCellState : Cell -> Board -> Bool
+getCellState (x, y) board =
+  -- if we are out of bounds = false
+  -- else if there is cell a position = Value of position.
+  case Array.get x board of
+    Nothing -> False
+    Just innerArray ->
+        case Array.get y innerArray of
+            Nothing -> False
+            Just value -> value
+
+getNumberAliveNeighbors : Cell -> Board -> Int
+getNumberAliveNeighbors cell board =
+  getNeighbors cell board
+    |> List.filter (\a -> getCellState cell board)
+    |> List.length
+
+checkLifeNextTurn : Cell -> Board -> Bool
+checkLifeNextTurn cell board =
+  if (getCellState cell board) then
+    ((getNumberAliveNeighbors cell board == 2) ||
+      (getNumberAliveNeighbors cell board == 3))
+  else
+    (getNumberAliveNeighbors cell board == 3)
+
+life : Cell -> Board -> Board
+life (x, y) board =
+  let
+    existingRow =
+      Array.get x board
+        |> Maybe.withDefault (Array.repeat (Array.length board) False)
+  in
+    board
+      |> Array.set x
+        (existingRow
+          |> Array.set y True)
+
+nextGeneration : Board -> Board
+nextGeneration board  =
+  let
+    neighbors cell row =
+      row
+        |> Array.indexedMap (\x _ -> checkLifeNextTurn ( cell, x ) board)
+  in
+    board
+      |> Array.indexedMap neighbors
+
+initBoard : Int -> Board
+initBoard size =
+  Array.repeat size (Array.repeat size False)
+
+
+-- Model
+type alias Board =
+  Array (Array Bool)
+
+type alias Cell = (Int, Int)
+
+type alias Model =
+  { board : Board
+  , generation : Int
+  , aliveCells : Int
+  }
+
+firstBoard = initBoard 10
+startModel =
+  { board = firstBoard
+  , generation = 0
+  , alivecells = 0
+  }
+
+
+-- Update
+type Msg
+  = Next Board
+  | Play Board
+  | Pause Board
+
+update : Msg -> Board -> Board
+update msg board =
+  case msg of
+    Next board -> nextGeneration board
+    Play board -> nextGeneration board
+    Pause board -> nextGeneration board
+
+
+-- View
+
+viewSquare : Bool -> Html Msg
+viewSquare bool =
+  let
+    colour = if (bool == True) then "black" else "white"
+  in td [ style [("height", "20px"),("width", "20px"),("background-color","colour")]]
+    []
+
+viewInnerArray : Array Bool -> Html Msg
+viewInnerArray array =
+  tr [] <|
+    List.map viewSquare (Array.toList array)
+
+viewBoard : Board -> Html Msg
+viewBoard model =
+  table [] <|
+    List.map viewInnerArray (Array.toList model)
+
+view : Model -> Html Msg
+view model =
+  div []
+      [h1 []
+        [text ("Conway's Game of Life")]
+      , button
+          [ type_ "button"
+          , onClick Next]
+          [ text "Next"]
+
+      , button
+          [ type_ "button"
+          , onClick Play]
+          [ text "Play"]
+
+      , button
+          [ type_ "button"
+          , onClick Pause]
+          [ text "Pause"]
+      ]
+
+main : Program Never Model Msg
+main =
+  Html.beginnerProgram
+  { model = startModel
+  , view = view
+  , update = update
+  }
---
+++ plan.md
@@ -0,0 +1,22 @@
+# Planning Conway Game of Life App
+
+## Model
+
+The model is composed by a list of boolean values, wich can be true or false,
+depending if the cell at that position is alive or not.
+The cells, are considered to be in a certain cordinates of the grid
+(x,y) so we can acces to the neighbors of it.
+
+## Update
+
+The desired actions for our app, are to get the next state of the board.
+It is also desired to implement a play/pause button.
+
+## View
+
+The logical sections of our app are:
+
+* Main View
+  * Button section
+  * Grid section
+  
